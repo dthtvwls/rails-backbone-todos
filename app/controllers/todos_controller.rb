609cf23ab44170ed1bd2286ccc2b3b1b8cc797b3
Joshua Stauter <joshua@joshuastauter.com>
2012-04-14 01:03:35 -0400

    respond_with

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,83 +1,40 @@
 class TodosController < ApplicationController
-  # GET /todos
-  # GET /todos.json
+  respond_to :html, :json
+
   def index
     @todos = Todo.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @todos }
-    end
+    respond_with @todos
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
   def show
-    @todo = Todo.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @todo }
-    end
+    @todo = Todo.find params[:id]
+    respond_with @todo
   end
 
-  # GET /todos/new
-  # GET /todos/new.json
   def new
     @todo = Todo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @todo }
-    end
+    respond_with @todo
   end
 
-  # GET /todos/1/edit
   def edit
-    @todo = Todo.find(params[:id])
+    @todo = Todo.find params[:id]
   end
 
-  # POST /todos
-  # POST /todos.json
   def create
-    @todo = Todo.new(params[:todo])
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render json: @todo, status: :created, location: @todo }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo = Todo.new params[:todo]
+    @todo.save
+    respond_with @todo
   end
 
-  # PUT /todos/1
-  # PUT /todos/1.json
   def update
-    @todo = Todo.find(params[:id])
-
-    respond_to do |format|
-      if @todo.update_attributes(params[:todo])
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo = Todo.find params[:id]
+    @todo.update_attributes params[:todo]
+    respond_with @todo
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
-    @todo = Todo.find(params[:id])
+    @todo = Todo.find params[:id]
     @todo.destroy
-
-    respond_to do |format|
-      format.html { redirect_to todos_url }
-      format.json { head :no_content }
-    end
+    head :ok
   end
 end
